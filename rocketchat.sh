d2e2ee939190f64bcc8e1961d5b1f9662e046a48
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-08-29 16:55:18 -0400

    Made Web Hook URL customizable

--- rocketchat.sh
+++ rocketchat.sh
@@ -6,20 +6,23 @@
 # Forked and enhanced from https://github.com/Open-Future-Belgium/zabbix-Rocket.Chat
 
 # Rocket.Chat incoming web-hook URL and user name
-url='http://rocketchat-rocketchat.apps.mon.example.opentlc.com/hooks/GEomjxsuxCMwszhKx/c4uakRKZXS7iC78fCiZsxjfwvLG7cnRv7i3ecjk6ei82p2Ku'
+#url='http://rocketchat-rocketchat.apps.mon.example.opentlc.com/hooks/GEomjxsuxCMwszhKx/c4uakRKZXS7iC78fCiZsxjfwvLG7cnRv7i3ecjk6ei82p2Ku'
 LOGFILE="/var/log/zabbix/zabbix-rocketchat.log"
 
 ## Values received by this script:
-# Subject = $1 (usually either OK or PROBLEM)
-# Message = $2 (whatever message the Zabbix action sends,
-#           preferably something like "Zabbix server is
-#           unreachable for 5 minutes - Zabbix
-#           server (127.0.0.1)")
-subject=$1
-message=$2
+# webhook = $1 The WebHook URL for the Rocket.Chat Channel
+# Subject = $2 usually either OK or PROBLEM
+# Message = $3 whatever message the Zabbix action sends,
+#              preferably something like "Zabbix server is
+#              unreachable for 5 minutes - Zabbix
+#              server (127.0.0.1)"
+webhook=$1
+subject=$2
+message=$3
 
 echo "****** New Alert received ******" >>${LOGFILE}
 echo "Sending Rocket.Chat Alert: Subject=${subject}; Message=${message}" >>${LOGFILE}
+echo "  WebHook: ${webhook}" >>${LOGFILE}
 
 # Set default values for emoji and color
 icon_emoji=":interrobang:"   # Exclamatin/Question Mark
@@ -44,9 +47,9 @@ payload="{\"text\": \"Zabbix Alert\", \"emoji\": \"${icon_emoji}\", \"attachment
 
 # Log alert
 echo "Sending Request to Rocket.Chat:" >>${LOGFILE}
-echo "  curl -X POST -H 'Content-Type: application/json' --data \"${payload}\" $url" >>${LOGFILE}
+echo "  curl -X POST -H 'Content-Type: application/json' --data \"${payload}\" $webhook" >>${LOGFILE}
 
 # Send Payload to the Rocket.Chat Server
-curl -X POST -H 'Content-Type: application/json' --data "${payload}" $url 2>&1 >>${LOGFILE}
+curl -X POST -H 'Content-Type: application/json' --data "${payload}" $webhook 2>&1 >>${LOGFILE}
 
 echo "\n****** Alert sent ******" >>${LOGFILE}
